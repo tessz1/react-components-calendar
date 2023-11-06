7ee6803fc8e85d4ce64f97ad3a3c9ae21977ec1b
Danila Kalinichev <tess4184@gmail.com>
2023-11-06 07:54:35 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './components/App'; 
 import reportWebVitals from './reportWebVitals';
+
+const currentDate = new Date(2017, 2, 8);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App date={currentDate} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
